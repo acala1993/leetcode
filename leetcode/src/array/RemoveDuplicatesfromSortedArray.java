9730735b2cfd97e3b8bd9142365f773e9422c75d
zhangli <zhangli@bigo.sg>
2020-04-25 20:07:45 +0800

    2020-4-25

--- leetcode/src/array/RemoveDuplicatesfromSortedArray.java
+++ leetcode/src/array/RemoveDuplicatesfromSortedArray.java
@@ -6,21 +6,30 @@ import java.util.Arrays;
 
 public class RemoveDuplicatesfromSortedArray {
     public static void main(String[] args) {
-        int[] arr = {1,1,2,3,3,3};
+        int[] arr = {1, 1, 2, 3, 3, 3};
         ArrayUtils.printArray(removeDuplicates(arr));
     }
 
     private static int[] removeDuplicates(int[] arr) {
-        int start = 0;
-        int end = 1;
-        int count = 1;
-        while (end < arr.length) {
-            if (arr[end] != arr[start]) {
-                arr[count++] = arr[end];
-                start = end;
+//        int start = 0;
+//        int end = 1;
+//        int count = 1;
+//        while (end < arr.length) {
+//            if (arr[end] != arr[start]) {
+//                arr[count++] = arr[end];
+//                start = end;
+//            }
+//            end++;
+//        }
+//        return Arrays.copyOf(arr, count);
+        int count = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[i - count] = arr[i];
+            }else {
+                count++;
             }
-            end++;
         }
-        return Arrays.copyOf(arr, count);
+        return Arrays.copyOf(arr, arr.length - count);
     }
 }
