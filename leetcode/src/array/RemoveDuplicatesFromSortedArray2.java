c2a7bd22c8c9842b8fdbebd48c8ed9b53a6365b8
zhangli <zhangli@bigo.sg>
2020-04-25 20:20:10 +0800

    2020-4-25

---
+++ leetcode/src/array/RemoveDuplicatesFromSortedArray2.java
@@ -0,0 +1,31 @@
+package array;
+
+import utils.ArrayUtils;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 2, 3, 3, 3};
+        ArrayUtils.printArray(removeDuplicates2(arr));
+    }
+
+    private static int[] removeDuplicates2(int[] arr) {
+        int count = 0;
+        int currCount = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[i - count] = arr[i];
+                currCount = 1;
+            } else {
+                if (currCount > 1){
+                    count++;
+                    currCount++;
+                } else{
+                    currCount++;
+                }
+            }
+        }
+        return Arrays.copyOf(arr, arr.length - count);
+    }
+}
